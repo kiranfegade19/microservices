7b90680c6edf16485da0f404b0c61518a515c6f6
kiranfegade19 <kiran.fegade19@gmail.com>
2024-06-05 02:02:51 +0530

    Live config changes

--- SmallBank/users/src/main/java/com/org/users/users/dto/ConfigurationEnvironmentCheckDto.java
+++ SmallBank/users/src/main/java/com/org/users/users/dto/ConfigurationEnvironmentCheckDto.java
@@ -3,8 +3,10 @@ package com.org.users.users.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter @Setter
+@Component
 @ConfigurationProperties(prefix = "data")
 public class ConfigurationEnvironmentCheckDto {
 
