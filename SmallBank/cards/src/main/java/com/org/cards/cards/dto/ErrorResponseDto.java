147dd4adb9f8bc160442345d603684183c211ce8
kiranfegade19 <kiran.fegade19@gmail.com>
2024-06-15 23:34:49 +0530

    Updating @Transactional annotation

--- SmallBank/cards/src/main/java/com/org/cards/cards/dto/ErrorResponseDto.java
+++ SmallBank/cards/src/main/java/com/org/cards/cards/dto/ErrorResponseDto.java
@@ -1,7 +1,14 @@
 package com.org.cards.cards.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ErrorResponseDto {
 
     private String apiPath;
