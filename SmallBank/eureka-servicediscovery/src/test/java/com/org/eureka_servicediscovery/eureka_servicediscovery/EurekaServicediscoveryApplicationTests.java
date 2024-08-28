b7a96ea4ebe81634b2c79992804f653fb21bef62
kiranfegade19 <kiran.fegade19@gmail.com>
2024-08-28 12:22:36 +0530

    Updating Eureka Service Discovery module

---
+++ SmallBank/eureka-servicediscovery/src/test/java/com/org/eureka_servicediscovery/eureka_servicediscovery/EurekaServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.eureka_servicediscovery.eureka_servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
